8f0061ba8b5937672cd5a4223b486966103722e5
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2024-11-26 16:45:33 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.97 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -3,8 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        int[] answer = new int[5];
-        System.arraycopy(num_list, 0, answer, 0, 5);
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
