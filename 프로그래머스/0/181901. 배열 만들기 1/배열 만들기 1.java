b693f35f53356f3fde6750514ba90e083bcd0e48
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2024-11-29 15:40:28 +0900

    [level 0] Title: 배열 만들기 1, Time: 3.97 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        return IntStream.rangeClosed(1, n / k)
+            .map(i -> k * i)
+            .toArray();
+    }
+}
