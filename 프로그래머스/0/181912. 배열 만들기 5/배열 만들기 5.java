a381bdbf9ca381b9d7da41c293f7b6259c8fe95a
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2024-12-12 15:20:27 +0900

    [level 0] Title: 배열 만들기 5, Time: 8.89 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        return Arrays.stream(intStrs)
+            .mapToInt(str -> Integer.parseInt(str.substring(s, s +l)))
+            .filter(i -> i > k)
+            .toArray();
+    }
+}
