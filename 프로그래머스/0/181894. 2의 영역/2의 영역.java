20bbb66fd9d2996c30804a498a9cd80074dcb5cb
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2024-12-23 12:51:01 +0900

    [level 0] Title: 2의 영역, Time: 0.92 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                start = i;
+                break;
+            }
+        }
+
+        if (start == -1) {
+            return new int[]{-1};
+        }
+
+        for (int i = arr.length - 1; i >= start; i--) {
+            if (arr[i] == 2) {
+                end = i;
+                break;
+            }
+        }
+
+        int[] answer = new int[end - start + 1];
+        System.arraycopy(arr, start, answer, 0, end - start + 1);
+        return answer;
+    }
+}
