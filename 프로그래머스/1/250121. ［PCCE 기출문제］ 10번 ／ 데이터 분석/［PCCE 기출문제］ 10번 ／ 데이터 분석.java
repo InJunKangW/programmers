87dae49c89dcd61e156f0410577622b3b0b2b008
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2025-02-13 14:17:24 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 4.21 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,91 +1,71 @@
 import java.util.*;
 
-class Data{
-    private int code;
-    private int date;
-    private int maximum;
-    private int remain;
-    
-    public Data(int[] data){
-        code = data[0];
-        date = data[1];
-        maximum = data[2];
-        remain = data[3];
-    }
-    
-    public int getCode(){return code;}
-    public int getDate(){return date;}
-    public int getMaximum(){return maximum;}
-    public int getRemain(){return remain;}
-    
-    public boolean match(String ext, int val_ext){
-        switch(ext){
-                case "code":
-                    return code < val_ext;
-                case "date":
-                    return date < val_ext;
-                case "maximum":
-                    return maximum < val_ext;
-                case "remain" :
-                    return remain < val_ext;
-                default :
-                    return true;
-        }
-    }
-    
-    public int[] makeArray(){
-        int [] result = {code, date, maximum, remain};
-        return result;
-    }
-}
-
-class DataComparator implements Comparator<Data> {
-    private final String sort_by;
-    
-    public DataComparator(String sort_by){
-        this.sort_by = sort_by;
-    }
-    
-    @Override
-    public int compare(Data data1, Data data2){
-        switch(sort_by){
-            case "code":
-                return data1.getCode() - data2.getCode();
-            case "date":
-                return data1.getDate() - data2.getDate();
-            case "maximum":
-                return data1.getMaximum() - data2.getMaximum();
-            case "remain" :
-                return data1.getRemain() - data2.getRemain();
-            default :
-                return 0;
-        }
-    }
-}
-
 class Solution {
-    public int[] makeArray(Data data){
-        int [] result = {data.getCode()};
-        return result;
-    }
-    
-    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+    private class Data{
+        public int code;
+        public int date;
+        public int maximum;
+        public int remain;
         
-        List<Data> dataList = new ArrayList<>();
+        public Data(int[] dataArr){
+            this.code = dataArr[0];
+            this.date = dataArr[1];
+            this.maximum = dataArr[2];
+            this.remain = dataArr[3];
+        }
         
-        for(int[] array : data){
-            Data personalData = new Data(array);
-            if(personalData.match(ext, val_ext)){
-                dataList.add(personalData);
+        public int[] toArray(){
+            return new int[] {code, date, maximum, remain};
+        }
+        
+        public Comparable getSortBy(String sort_by) {
+            switch (sort_by) {
+                case "code": return this.code;
+                case "date": return this.date;
+                case "maximum": return this.maximum;
+                case "remain": return this.remain;
+                default: return null;
             }
         }
-        dataList.sort(new DataComparator(sort_by));
+    }
+    
+    
+    
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        List<Data> dataList = new ArrayList<>();
         
-        int[][] answer = new int[dataList.size()][4];
-        for(int i = 0; i < dataList.size(); i++) {
-            answer[i] = dataList.get(i).makeArray();
+        for (int[] dataArr : data) {
+            dataList.add(new Data(dataArr));
+        }
+        List<Data> filteredData = new ArrayList<>();
+        for (Data d : dataList) {
+            switch (ext) {
+                case "code":
+                    if (d.code < val_ext) filteredData.add(d);
+                    break;
+                case "date":
+                    if (d.date < val_ext) filteredData.add(d);
+                    break;
+                case "maximum":
+                    if (d.maximum < val_ext) filteredData.add(d);
+                    break;
+                case "remain":
+                    if (d.remain < val_ext) filteredData.add(d);
+                    break;
+            }
         }
         
+        Collections.sort(filteredData, (d1, d2) -> {
+            Comparable val1 = d1.getSortBy(sort_by);
+            Comparable val2 = d2.getSortBy(sort_by);
+            return val1.compareTo(val2);
+        });
+        
+        int[][] answer = new int[filteredData.size()][4];
+        for (int i = 0; i < filteredData.size(); i++) {
+            Data d = filteredData.get(i);
+            answer[i] = d.toArray();
+        }
         return answer;
     }
 }
